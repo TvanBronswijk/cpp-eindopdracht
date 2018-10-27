f42987cb8429457d55b65658a3b424112ac14bdc
Tobi Bronswijk <294604@student.fontys.nl>
2018-10-27 14:28:42 +0200

    updated map

--- cpp-eindopdracht/mapview.cpp
+++ cpp-eindopdracht/mapview.cpp
@@ -7,28 +7,47 @@ MapView::MapView(GameContext * context) : View(context)
 
 std::ostream & MapView::display()
 {
+	std::cout
+		<< "~~~THE DUNGEON~~~"
+		<< std::endl
+		<< std::endl;
+
 	Dungeon* dungeon = this->context->gamestate->get_dungeon();
+	std::cout << "#";
+	for (size_t y = 0; y < dungeon->height(); y++)
+		std::cout << "##";
+	std::cout << "#";
+	std::cout << std::endl;
 	for (size_t y = 0; y < dungeon->height(); y++) {
+		std::cout << "#";
 		for (size_t x = 0; x < dungeon->width(); x++) {
 			if (dungeon->coord(x, y)->visited)
 				std::cout << "X";
 			else
-				std::cout << " ";
+				std::cout << ".";
 
 			if (dungeon->coord(x, y)->right != nullptr && (dungeon->coord(x, y)->visited || dungeon->coord(x, y)->right->visited))
 				std::cout << "-";
 			else
 				std::cout << " ";
 		}
+		std::cout << "#";
 		std::cout << std::endl;
+		std::cout << "#";
 		for (size_t x = 0; x < dungeon->width(); x++) {
 			if (dungeon->coord(x, y)->up != nullptr && (dungeon->coord(x, y)->visited || dungeon->coord(x, y)->up->visited))
 				std::cout << "| ";
 			else
 				std::cout << "  ";
 		}
+		std::cout << "#";
 		std::cout << std::endl;
 	}
+	std::cout << "#";
+	for (size_t y = 0; y < dungeon->height(); y++)
+		std::cout << "##";
+	std::cout << "#";
+	std::cout << std::endl;
 
 	return std::cout
 		<< std::endl
